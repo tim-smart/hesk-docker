c6c4117d93998a02088a3fe3de1c220d221ffa72
Tim Smart <tim.smart@arisechurch.com>
2021-09-07 11:07:37 +1200

    Add getenv_string function to settings

--- hesk_settings.inc.php
+++ hesk_settings.inc.php
@@ -1,5 +1,15 @@
 <?php
 // Settings file for HESK 3.2.3
+function getenv_string($name) {
+    $value = getenv($name);
+
+    if ($value) {
+        return $value;
+    }
+
+    return null;
+}
+
 function getenv_number($name) {
     $value = getenv($name);
 
@@ -7,20 +17,20 @@ function getenv_number($name) {
         return intval($value);
     }
 
-    return false;
+    return null;
 }
 
 // ==> GENERAL
 
 // --> General settings
-$hesk_settings['site_title']=getenv('HESK_SITE_TITLE') ?: 'Website';
-$hesk_settings['site_url']=getenv('HESK_SITE_URL') ?: 'http://www.example.com';
-$hesk_settings['hesk_title']=getenv('HESK_HESK_TITLE') ?: 'Help Desk';
-$hesk_settings['hesk_url']=getenv('HESK_HESK_URL') ?: 'http://www.example.com/helpdesk';
-$hesk_settings['webmaster_mail']=getenv('HESK_WEBMASTER_MAIL') ?: 'support@example.com';
-$hesk_settings['noreply_mail']=getenv('HESK_NOREPLY_MAIL') ?: 'support@example.com';
-$hesk_settings['noreply_name']=getenv('HESK_NOREPLY_NAME') ?: 'Help Desk';
-$hesk_settings['site_theme']=getenv('HESK_SITE_THEME') ?: 'hesk3';
+$hesk_settings['site_title']=getenv_string('HESK_SITE_TITLE') ?? 'Website';
+$hesk_settings['site_url']=getenv_string('HESK_SITE_URL') ?? 'http://www.example.com';
+$hesk_settings['hesk_title']=getenv_string('HESK_HESK_TITLE') ?? 'Help Desk';
+$hesk_settings['hesk_url']=getenv_string('HESK_HESK_URL') ?? 'http://www.example.com/helpdesk';
+$hesk_settings['webmaster_mail']=getenv_string('HESK_WEBMASTER_MAIL') ?? 'support@example.com';
+$hesk_settings['noreply_mail']=getenv_string('HESK_NOREPLY_MAIL') ?? 'support@example.com';
+$hesk_settings['noreply_name']=getenv_string('HESK_NOREPLY_NAME') ?? 'Help Desk';
+$hesk_settings['site_theme']=getenv_string('HESK_SITE_THEME') ?? 'hesk3';
 $hesk_settings['admin_css']=0;
 $hesk_settings['admin_css_url']='https://www.example.com/hesk-style.css';
 
@@ -32,11 +42,11 @@ $hesk_settings['languages']=array(
 );
 
 // --> Database settings
-$hesk_settings['db_host']=getenv('HESK_DB_HOST') ?: 'localhost';
-$hesk_settings['db_name']=getenv('HESK_DB_NAME') ?: 'hesk';
-$hesk_settings['db_user']=getenv('HESK_DB_USER') ?: 'test';
-$hesk_settings['db_pass']=getenv('HESK_DB_PASS') ?: 'test';
-$hesk_settings['db_pfix']=getenv('HESK_DB_PFIX') ?: 'hesk_';
+$hesk_settings['db_host']=getenv_string('HESK_DB_HOST') ?? 'localhost';
+$hesk_settings['db_name']=getenv_string('HESK_DB_NAME') ?? 'hesk';
+$hesk_settings['db_user']=getenv_string('HESK_DB_USER') ?? 'test';
+$hesk_settings['db_pass']=getenv_string('HESK_DB_PASS') ?? 'test';
+$hesk_settings['db_pfix']=getenv_string('HESK_DB_PFIX') ?? 'hesk_';
 
 
 // ==> HELP DESK
@@ -78,11 +88,11 @@ $hesk_settings['cat_show_select']=15;
 $hesk_settings['staff_ticket_formatting']=0;
 
 // --> SPAM Prevention
-$hesk_settings['secimg_use']=getenv_number('HESK_SECIMG_USE') ?: 1;
-$hesk_settings['secimg_sum']=getenv('HESK_SECIMG_SUM') ?: '13N4BRS4WW';
-$hesk_settings['recaptcha_use']=getenv_number('HESK_RECAPTCHA_USE') ?: 0;
-$hesk_settings['recaptcha_public_key']=getenv('HESK_RECAPTCHA_PUBLIC_KEY') ?: '';
-$hesk_settings['recaptcha_private_key']=getenv('HESK_RECAPTCHA_PRIVATE_KEY') ?: '';
+$hesk_settings['secimg_use']=getenv_number('HESK_SECIMG_USE') ?? 1;
+$hesk_settings['secimg_sum']=getenv_string('HESK_SECIMG_SUM') ?? '13N4BRS4WW';
+$hesk_settings['recaptcha_use']=getenv_number('HESK_RECAPTCHA_USE') ?? 0;
+$hesk_settings['recaptcha_public_key']=getenv_string('HESK_RECAPTCHA_PUBLIC_KEY') ?? '';
+$hesk_settings['recaptcha_private_key']=getenv_string('HESK_RECAPTCHA_PRIVATE_KEY') ?? '';
 $hesk_settings['question_use']=0;
 $hesk_settings['question_ask']='Type <i>PB6YM</i> here to fight SPAM:';
 $hesk_settings['question_ans']='PB6YM';
@@ -131,14 +141,14 @@ $hesk_settings['kb_related']=5;
 // ==> EMAIL
 
 // --> Email sending
-$hesk_settings['smtp']=getenv_number('HESK_SMTP') ?: 0;
-$hesk_settings['smtp_host_name']=getenv('HESK_SMTP_HOST_NAME') ?: 'mail.example.com';
-$hesk_settings['smtp_host_port']=getenv_number('HESK_SMTP_HOST_PORT') ?: 25;
-$hesk_settings['smtp_timeout']=getenv_number('HESK_SMTP_TIMEOUT') ?: 20;
-$hesk_settings['smtp_ssl']=getenv_number('HESK_SMTP_SSL') ?: 0;
-$hesk_settings['smtp_tls']=getenv_number('HESK_SMTP_TLS') ?: 0;
-$hesk_settings['smtp_user']=getenv('HESK_SMTP_USER') ?: '';
-$hesk_settings['smtp_password']=getenv('HESK_SMTP_PASSWORD') ?: '';
+$hesk_settings['smtp']=getenv_number('HESK_SMTP') ?? 0;
+$hesk_settings['smtp_host_name']=getenv_string('HESK_SMTP_HOST_NAME') ?? 'mail.example.com';
+$hesk_settings['smtp_host_port']=getenv_number('HESK_SMTP_HOST_PORT') ?? 25;
+$hesk_settings['smtp_timeout']=getenv_number('HESK_SMTP_TIMEOUT') ?? 20;
+$hesk_settings['smtp_ssl']=getenv_number('HESK_SMTP_SSL') ?? 0;
+$hesk_settings['smtp_tls']=getenv_number('HESK_SMTP_TLS') ?? 0;
+$hesk_settings['smtp_user']=getenv_string('HESK_SMTP_USER') ?? '';
+$hesk_settings['smtp_password']=getenv_string('HESK_SMTP_PASSWORD') ?? '';
 
 // --> Email piping
 $hesk_settings['email_piping']=0;
@@ -154,15 +164,15 @@ $hesk_settings['pop3_user']='';
 $hesk_settings['pop3_password']='';
 
 // --> IMAP Fetching
-$hesk_settings['imap']=getenv_number('HESK_IMAP') ?: 0;
-$hesk_settings['imap_job_wait']=getenv_number('HESK_IMAP_JOB_WAIT') ?: 15;
-$hesk_settings['imap_host_name']=getenv('HESK_IMAP_HOST_NAME') ?: 'mail.example.com';
-$hesk_settings['imap_host_port']=getenv_number('HESK_IMAP_HOST_PORT') ?: 993;
-$hesk_settings['imap_enc']=getenv('HESK_IMAP_ENC') ?: 'ssl';
-$hesk_settings['imap_noval_cert']=getenv_number('HESK_IMAP_NOVAL_CERT') ?: 0;
-$hesk_settings['imap_keep']=getenv_number('HESK_IMAP_KEEP') ?: 0;
-$hesk_settings['imap_user']=getenv('HESK_IMAP_USER') ?: '';
-$hesk_settings['imap_password']=getenv('HESK_IMAP_PASSWORD') ?: '';
+$hesk_settings['imap']=getenv_number('HESK_IMAP') ?? 0;
+$hesk_settings['imap_job_wait']=getenv_number('HESK_IMAP_JOB_WAIT') ?? 15;
+$hesk_settings['imap_host_name']=getenv_string('HESK_IMAP_HOST_NAME') ?? 'mail.example.com';
+$hesk_settings['imap_host_port']=getenv_number('HESK_IMAP_HOST_PORT') ?? 993;
+$hesk_settings['imap_enc']=getenv_string('HESK_IMAP_ENC') ?? 'ssl';
+$hesk_settings['imap_noval_cert']=getenv_number('HESK_IMAP_NOVAL_CERT') ?? 0;
+$hesk_settings['imap_keep']=getenv_number('HESK_IMAP_KEEP') ?? 0;
+$hesk_settings['imap_user']=getenv_string('HESK_IMAP_USER') ?? '';
+$hesk_settings['imap_password']=getenv_string('HESK_IMAP_PASSWORD') ?? '';
 
 // --> Email loops
 $hesk_settings['loop_hits']=5;
@@ -173,10 +183,10 @@ $hesk_settings['detect_typos']=1;
 $hesk_settings['email_providers']=array('aim.com','aol.co.uk','aol.com','att.net','bellsouth.net','blueyonder.co.uk','bt.com','btinternet.com','btopenworld.com','charter.net','comcast.net','cox.net','earthlink.net','email.com','facebook.com','fastmail.fm','free.fr','freeserve.co.uk','gmail.com','gmx.at','gmx.ch','gmx.com','gmx.de','gmx.fr','gmx.net','gmx.us','googlemail.com','hotmail.be','hotmail.co.uk','hotmail.com','hotmail.com.ar','hotmail.com.mx','hotmail.de','hotmail.es','hotmail.fr','hushmail.com','icloud.com','inbox.com','laposte.net','lavabit.com','list.ru','live.be','live.co.uk','live.com','live.com.ar','live.com.mx','live.de','live.fr','love.com','lycos.com','mac.com','mail.com','mail.ru','me.com','msn.com','nate.com','naver.com','neuf.fr','ntlworld.com','o2.co.uk','online.de','orange.fr','orange.net','outlook.com','pobox.com','prodigy.net.mx','qq.com','rambler.ru','rocketmail.com','safe-mail.net','sbcglobal.net','t-online.de','talktalk.co.uk','tiscali.co.uk','verizon.net','virgin.net','virginmedia.com','wanadoo.co.uk','wanadoo.fr','yahoo.co.id','yahoo.co.in','yahoo.co.jp','yahoo.co.kr','yahoo.co.uk','yahoo.com','yahoo.com.ar','yahoo.com.mx','yahoo.com.ph','yahoo.com.sg','yahoo.de','yahoo.fr','yandex.com','yandex.ru','ymail.com');
 
 // --> Notify customer when
-$hesk_settings['notify_new']=1;
-$hesk_settings['notify_skip_spam']=1;
+$hesk_settings['notify_new']=getenv_number('HESK_NOTIFY_NEW') ?? 1;
+$hesk_settings['notify_skip_spam']=getenv_number('HESK_NOTIFY_SKIP_SPAM') ?? 1;
 $hesk_settings['notify_spam_tags']=array('Spam?}','***SPAM***','[SPAM]','SPAM-LOW:','SPAM-MED:');
-$hesk_settings['notify_closed']=1;
+$hesk_settings['notify_closed']=getenv_number('HESK_NOTIFY_CLOSED') ?? 1;
 
 // --> Other
 $hesk_settings['strip_quoted']=1;
@@ -199,7 +209,7 @@ $hesk_settings['updatedformat']=2;
 // ==> MISC
 
 // --> Date & Time
-$hesk_settings['timezone']=getenv('HESK_TIMEZONE') ?: 'UTC';
+$hesk_settings['timezone']=getenv_string('HESK_TIMEZONE') ?? 'UTC';
 $hesk_settings['timeformat']='Y-m-d H:i:s';
 $hesk_settings['time_display']='1';
 
